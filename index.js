05bc342b1af1a474ec2a2c83adacbcc95b8ae14a
Airidas0 <airidas.se@gmail.com>
2024-08-07 21:23:17 +0300

    Made it so it also includes the rounded expression

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ document.getElementById("mysubmit").onclick = function(){
     radius = document.getElementById("myradius").value;
     result = 2 * PI * radius;
     document.getElementById("myresult").textContent = `Apskritimo ilgis: ${result}cm`;
+    document.getElementById("apvalintas").textContent = `Suapvalinus: ${Math.round(result)}cm`;
 }
+
+
 
 
