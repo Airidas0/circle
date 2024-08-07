80705e57bc0c44a2c990e442151d10eeedd93f0c
Airidas0 <airidas.se@gmail.com>
2024-08-07 18:43:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const PI = 3.141592653
+let radius;
+let result;
+document.getElementById("mysubmit").onclick = function(){
+    radius = document.getElementById("myradius").value;
+    result = 2 * PI * radius;
+    document.getElementById("myresult").textContent = `Apskritimo ilgis: ${result}cm`;
+}
+
+
